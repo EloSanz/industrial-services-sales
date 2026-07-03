69588509ba025aa9ef69c4dac5d8538564d93c08
EloSanz <sanzeliseo@yahoo.com.ar>
2026-07-03 13:54:59 -0300

    add basepath

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  // Si usas el componente <Image> de Next.js, debes desactivar la optimización por defecto
-  // ya que esta requiere un servidor Node.js corriendo para optimizar imágenes al vuelo.
+  basePath: '/sales-industry',
   images: {
     unoptimized: true,
   },
